4e4a00a7ee9b7e770733935927050205ad6382aa
komal <kkumari@kloudrac.com>
2025-08-29 14:33:30 +0530

    Updated inventory controller, route, and entity

--- src/api/v1/routers/inventoryroute/inventoryRoute.ts
+++ src/api/v1/routers/inventoryroute/inventoryRoute.ts
@@ -9,9 +9,12 @@ router.use(AccessTokenService.validateTokenMiddleware!(JwtTokenTypes.AUTH_TOKEN)
 
 router.post("/create", (req, res) => inventoryController.create(req, res));
 router.get("/", (req, res) => inventoryController.getAll(req, res));
+router.get("/getSummary", (req, res) => inventoryController.getSummary(req, res));
+router.get("/getLowStock", (req, res) => inventoryController.getLowStock(req, res));
 router.get("/:id", (req, res) => inventoryController.getById(req, res));
 router.put("/:id", (req, res) => inventoryController.update(req, res));
 router.delete("/:id", (req, res) => inventoryController.delete(req, res));
+
 
 
 export default router;
